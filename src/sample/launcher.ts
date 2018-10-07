0abdeae5213b52208c311066859ce497528a8144
Stephen <sjames8888@gmail.com>
2018-10-07 22:50:16 +0100

    simplify launching of test demos

--- src/sample/launcher.ts
+++ src/sample/launcher.ts
@@ -3,13 +3,16 @@ import { JsFixWinstonLogFactory } from '../config/js-fix-winston-log-factory'
 import { WinstonLogger } from '../config/winston-logger'
 import { IJsFixConfig } from '../config/js-fix-config'
 import { SessionMsgFactory } from '../transport/session-msg-factory'
+import { IJsFixLogger } from '../config/js-fix-logger'
 import { makeConfig } from '../transport/make-config'
 
 const root = '../../'
 const logFactory = new JsFixWinstonLogFactory(WinstonLogger.consoleOptions('info'))
 
 export abstract class Launcher {
+  private readonly logger: IJsFixLogger
   protected constructor (public readonly initiatorConfig: string, public readonly acceptorConfig: string) {
+    this.logger = logFactory.logger('launcher')
   }
 
   protected abstract getInitiator (config: IJsFixConfig): Promise<any>
@@ -17,7 +20,7 @@ export abstract class Launcher {
 
   public run () {
     return new Promise<any>((accept, reject) => {
-      const logger = logFactory.logger('app')
+      const logger = this.logger
       logger.info('launching ..')
       this.setup().then(() => {
         logger.info('.. done')
@@ -32,39 +35,16 @@ export abstract class Launcher {
   private async setup () {
     const clientDescription = require(path.join(root, this.initiatorConfig))
     const serverDescription = require(path.join(root, this.acceptorConfig))
+    this.logger.info('launching ..')
     const clientConfig = await
     makeConfig(clientDescription, logFactory, new SessionMsgFactory(clientDescription))
     const serverConfig = await
     makeConfig(serverDescription, logFactory, new SessionMsgFactory(serverDescription))
+    this.logger.info('create acceptor')
     const server = this.getAcceptor(serverConfig)
+    this.logger.info('create initiator')
     const client = this.getInitiator(clientConfig)
+    this.logger.info('launching ....')
     return Promise.all([server, client])
   }
 }
-
-/*
-async function app (sessionFactory: MakeFixSession, getInitiator: IGetFixSystem, getAcceptor: IGetFixSystem) {
-  const clientDescription = require(path.join(root, './data/session/test-http-initiator.json'))
-  const serverDescription = require(path.join(root, './data/session/test-http-acceptor.json'))
-  const clientConfig = await makeConfig(clientDescription, logFactory, new SessionMsgFactory(clientDescription))
-  const serverConfig = await makeConfig(serverDescription, logFactory, new SessionMsgFactory(serverDescription))
-  const server = getAcceptor(serverConfig, sessionFactory)
-  const client = getInitiator(clientConfig, sessionFactory)
-  return Promise.all([server, client])
-}
-
-export async function runner (sessionFactory: MakeFixSession,
-                              getInitiator: IGetFixSystem,
-                              getAcceptor: IGetFixSystem): Promise<any> {
-  return new Promise<any>((accept, reject) => {
-    const logger = logFactory.logger('app')
-    logger.info('launching ..')
-    app(sessionFactory, getInitiator, getAcceptor).then(() => {
-      logger.info('.. done')
-      accept()
-    }).catch((e: Error) => {
-      logger.error(e)
-      reject(e)
-    })
-  })
-} */
