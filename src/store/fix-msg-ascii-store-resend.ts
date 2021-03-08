8d20dbae2f7cbc6b4da8e573ffa31b270e83b21b
Stephen James <sjames8888@gmail.com>
2021-03-08 18:35:17 +0000

    start a test suite for replay request which will be used to handle lost messages or request for a replay

--- src/store/fix-msg-ascii-store-resend.ts
+++ src/store/fix-msg-ascii-store-resend.ts
@@ -3,20 +3,20 @@ import { ILooseObject } from '../collections/collection'
 import { IFixMsgStoreRecord } from './fix-msg-store-record'
 import { AsciiParser, MsgView } from '../buffer'
 import { IJsFixConfig } from '../config'
-import { IFixReplayRecord, FixReplayRecord } from './fix-replay-record'
+import { IFixResendRecord, FixResendRecord } from './fix-resend-record'
 import { MsgType } from '../types'
 
-export class FixMsgAsciiStoreReplay {
+export class FixMsgAsciiStoreResend {
   constructor (public readonly store: IFixMsgStore, public readonly config: IJsFixConfig) {
   }
 
-  public getReplayRequest (startSeq: number, endSeq: number): IFixReplayRecord[] {
+  public getResendRequest (startSeq: number, endSeq: number): IFixResendRecord[] {
 
     // need to cover request from start to end where any missing numbers are
     // included as gaps to allow vector of messages to be sent by the session
     // on a request
 
-    const arr: IFixReplayRecord[] = []
+    const arr: IFixResendRecord[] = []
     let seqNum = startSeq
     let beginGap = 0
     while (seqNum <= endSeq) {
@@ -38,7 +38,7 @@ export class FixMsgAsciiStoreReplay {
     return arr
   }
 
-  public createMsg (record: IFixMsgStoreRecord): IFixReplayRecord {
+  public createMsg (record: IFixMsgStoreRecord): IFixResendRecord {
     let res: ILooseObject = null
     const parser: AsciiParser = new AsciiParser(this.config.definitions, null, this.config.delimiter)
     parser.on('error', (e: Error) => {
@@ -51,10 +51,10 @@ export class FixMsgAsciiStoreReplay {
       res = null
     })
     parser.parseText(record.text)
-    return new FixReplayRecord(res, record.msgType, record.seqNum)
+    return new FixResendRecord(res, record.msgType, record.seqNum)
   }
 
-  public gap (beginGap: number, seqNum: number, arr: IFixReplayRecord[]) {
+  public gap (beginGap: number, seqNum: number, arr: IFixResendRecord[]) {
     if (beginGap > 0) {
       arr.push(this.sequenceReset(beginGap))
       if (seqNum > beginGap) {
@@ -63,7 +63,7 @@ export class FixMsgAsciiStoreReplay {
     }
   }
 
-  public sequenceReset (newSeq: number): IFixReplayRecord {
-    return new FixReplayRecord(this.config.factory.sequenceReset(newSeq), MsgType.SequenceReset, newSeq)
+  public sequenceReset (newSeq: number): IFixResendRecord {
+    return new FixResendRecord(this.config.factory.sequenceReset(newSeq), MsgType.SequenceReset, newSeq)
   }
 }
--- src/store/fix-resend-record.ts
+++ src/store/fix-resend-record.ts
@@ -1,12 +1,12 @@
 import { ILooseObject } from '../collections/collection'
 
-export interface IFixReplayRecord {
+export interface IFixResendRecord {
   readonly object: ILooseObject
   readonly msgType: string
   readonly seqNum: number
 }
 
-export class FixReplayRecord implements IFixReplayRecord {
+export class FixResendRecord implements IFixResendRecord {
   constructor (public readonly object: ILooseObject,
                public readonly msgType: string,
                public readonly seqNum: number) {
