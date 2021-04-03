372ab6fc47c5acccb3dae1b23f473b90e99f45b1
Stephen James <sjames8888@gmail.com>
2021-04-03 16:09:40 +0100

    add MD quickfix demo to samples showing usage of a quickfix style dictionary

---
+++ src/sample/tcp/qf-md/md-factory.ts
@@ -0,0 +1,34 @@
+import {
+  IMarketDataRequest,
+  MDEntryType,
+  SubscriptionRequestType } from '../../../types/FIX4.4/quickfix'
+
+export class MDFactory {
+  public static BidOfferRequest (symbol: string) {
+    return {
+      MDReqID: '1',
+      SubscriptionRequestType: SubscriptionRequestType.SnapshotPlusUpdates,
+      MarketDepth: 0,
+      MDReqGrp: {
+        NoMDEntryTypes: [
+          {
+            MDEntryType: MDEntryType.Bid
+          },
+          {
+            MDEntryType: MDEntryType.Offer
+          }
+        ]
+      },
+      InstrmtMDReqGrp: {
+        NoRelatedSym: [
+          {
+            Instrument: {
+              StrikeCurrency: 'USD',
+              Symbol: symbol
+            }
+          }
+        ]
+      }
+    } as IMarketDataRequest
+  }
+}
